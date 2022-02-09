59cc9b58ce95bfd58c161ea766c500e54ff09aae
Jacob D. Castro <jacob@jdc.dev>
2022-02-09 09:08:12 -0800

    update banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Banner = () => {
   return (
     <div className='bg-yellow-300 text-black text-center font-heading text-2xl p-2'>
-      Minting Begins on February 9th at 12pm EST
+      Minting LIVE NOW
     </div>
   );
 };
