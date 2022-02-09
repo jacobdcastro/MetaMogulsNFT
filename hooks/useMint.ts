1186e879e686f387741d7fb532158a46ff09becf
Jacob D. Castro <jacob@jdc.dev>
2022-02-09 07:22:42 -0800

    add working mint fn

--- hooks/useMint.ts
+++ hooks/useMint.ts
@@ -1,9 +1,8 @@
-import { providers, utils, BigNumber } from 'ethers';
+import { providers, Contract, utils, BigNumber } from 'ethers';
 import { useWeb3 } from '@3rdweb/hooks';
 import abi from '../config/abi.json';
 import CONFIG from '../config/config.json';
 import { useState } from 'react';
-import Web3EthContract from 'web3-eth-contract';
 
 const CONTRACT_ADDRESS = '0x5d6685C7BD265204ec9BDE279095CBF478165898';
 
@@ -12,42 +11,28 @@ export const useMint = () => {
   const { provider, address } = useWeb3();
 
   const getContract = () => {
-    // const signer = provider?.getSigner();
-    // @ts-ignore
-    Web3EthContract.setProvider(provider);
-    // @ts-ignore
-    return new Web3EthContract(abi, CONTRACT_ADDRESS);
+    const signer = provider?.getSigner();
+    return new Contract(CONTRACT_ADDRESS, abi, signer);
   };
 
-  const mintPresale = async (mintAmount = 1) => {
+  const mintPresale = async (mintAmount: number) => {
     const contract = getContract();
-    let cost = CONFIG.WEI_COST;
+    let cost = CONFIG.PRESALE_WEI_COST;
     let gasLimit = CONFIG.GAS_LIMIT;
     let totalCostWei = cost * mintAmount;
     let totalGasLimit = gasLimit * mintAmount;
     console.log('Cost: ', totalCostWei);
     console.log('Gas limit: ', totalGasLimit);
-    setLoading(true);
-
-    console.log({ mintAmount, totalGasLimit, CONTRACT_ADDRESS });
 
     try {
-      contract.methods
-        .mint(mintAmount)
-        .send({
-          gasLimit: totalGasLimit,
-          to: CONTRACT_ADDRESS,
-          from: address,
-          value: totalCostWei,
-        })
-        .once('error', err => {
-          console.log(err);
-          setLoading(false);
-        })
-        .then(receipt => {
-          console.log(receipt);
-          setLoading(false);
-        });
+      const tx = await contract.mint(mintAmount, {
+        value: totalCostWei.toString(),
+      });
+
+      setLoading(true);
+      await tx.wait();
+      console.log(tx);
+      setLoading(false);
     } catch (error) {
       console.error(error);
     }
